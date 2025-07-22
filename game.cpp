3596ba373829ff3c7f7378e1d95da6c2632d8055
Sebo1411 <sebastijan.krklec11@gmail.com>
2025-07-22 21:19:23 +0200

    cpu Perlin noise

--- game.cpp
+++ game.cpp
@@ -2,6 +2,9 @@
 #include <array>
 #include <thread>
 #include <functional>
+#include <random>
+#include <cassert>
+#include <cmath>
 
 #include "raylib.h"
 #include "raylib-cpp.hpp"
@@ -155,12 +158,12 @@ public:
 
     template <typename Rep, typename Period>
     void physics(std::chrono::duration<Rep, Period> deltaT) {
-        DEBUG_ONLY(std::cout << "Prije: \n" << *this << "\n\n";)
+        //DEBUG_ONLY(std::cout << "Prije: \n" << *this << "\n\n";)
         for (size_t i = 0; i < num; i++) {
             velocities[i] += (accelerations[i] * deltaT.count()) / std::remove_cvref_t<decltype(deltaT)>::period::den;
             positions[i] += (velocities[i] * deltaT.count()) / std::remove_cvref_t<decltype(deltaT)>::period::den;
         }
-        DEBUG_ONLY(std::cout << "Poslije: \n" << *this << "\n\n";)
+        //DEBUG_ONLY(std::cout << "Poslije: \n" << *this << "\n\n";)
     }
 };
 
@@ -241,6 +244,9 @@ public:
                 Vec2<double> n { start.y - end.y, end.x - start.x };
                 n.normalize();
                 circles.velocities[i] -= n * 2 * (circles.velocities[i] * n);
+
+                //to prevent balls getting stuck
+                circles.positions[i] += (circles.velocities[i] * 10) / std::chrono::steady_clock::duration::period::den;
             }
         }
     }
@@ -280,31 +286,139 @@ public:
     }
 };
 
-class Game {
-public:
-    Line line;
-    Background background;
-    raylib::Camera2D camera;
-
+class PerlinNoise {
 private:
-    GameThreadPool gtp;
+    std::vector<int> permutation;
+
+    std::vector<int>& FisherYatesShuffle(std::vector<int>& permutation) {
+        std::chrono::high_resolution_clock clock;
+        std::ranlux48 rEngine { static_cast<uint64_t>(clock.now().time_since_epoch().count()) };
+        for (size_t i = permutation.size() - 1; i > 0; i--) {
+            std::swap(permutation[rEngine() % i], permutation[i]);
+        }
+
+        DEBUG_ONLY(for (size_t i = 0; i < permutation.size(); i++) {
+            std::cout << permutation[i] << "\n";
+        })
+
+        return permutation;
+    }
+
+    int repeat = 0;
+
+    Vec2<double> randomGradient(int ix, int iy) {
+        // No precomputed gradients mean this works for any number of grid coordinates
+        const unsigned w = 8 * sizeof(unsigned);
+        const unsigned s = w / 2;
+        unsigned a = ix, b = iy;
+        a *= 3284157443;
+
+        b ^= a << s | a >> w - s;
+        b *= 1911520717;
+
+        a ^= b << s | b >> w - s;
+        a *= 2048419325;
+        double random = a * (PI / ~(~0u >> 1)); // in [0, 2*Pi]
+
+        return Vec2<double> {
+            std::sin(random), std::cos(random)
+        };
+    }
+
+    //dot prod distance and gradient vectors
+    double dotGridGradient(int ix, int iy, double x, double y) {
+        //gradient from integer coordinates: pseudo-random, deterministic, well distibuted
+        Vec2<double> gradient = randomGradient(ix, iy);
+
+        //distance vector
+        double dx = x - (double)ix;
+        double dy = y - (double)iy;
+
+        //dot product
+        return dx * gradient.x + dy * gradient.y;
+    }
+
+    double interpolate1(double t) {
+        return t * t * t * (t * (t * 6 - 15) + 10);
+    }
+
+    double interpolate2(double a0, double a1, double w) {
+        return (a1 - a0) * (3.0 - w * 2.0) * w * w + a0;
+    }
+
+    double CPUhelper(double x, double y) {
+        //grid cell corners
+        int x0 = (int)x;
+        int y0 = (int)y;
+        int x1 = x0 + 1;
+        int y1 = y0 + 1;
+
+        //interpolation weights
+        double sx = x - (double)x0;
+        double sy = y - (double)y0;
+
+        //compute and interpolate top 2 corners
+        double n0 = dotGridGradient(x0, y0, x, y);
+        double n1 = dotGridGradient(x1, y0, x, y);
+        double ix0 = interpolate2(n0, n1, sx);
+
+        //compute and interpolate bottom 2 corners
+        n0 = dotGridGradient(x0, y1, x, y);
+        n1 = dotGridGradient(x1, y1, x, y);
+        double ix1 = interpolate2(n0, n1, sx);
+
+        //interpolate between the two previously interpolated values, now in y
+        double value = interpolate2(ix0, ix1, sy);
+
+        return value;
+    }
 
 public:
+    void CPU(std::vector<uint8_t>& pixels, int width, int height) {
+        const int GRID_SIZE = 400;
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                int index = (y * width + x) * 4;
 
-    Game(raylib::Window& window) : line(window, window.GetWidth(), window.GetHeight()), background(window, raylib::Color { 31, 31, 30, 255 }),
-                                   camera({ 0, 0 }, {0,0}, 0, 0), gtp(window, line, background, [this]() { this->draw(); }, background.color) {
-        
+                double val = 0;
+
+                double freq = 1;
+                double amp = 1;
+
+                for (int i = 0; i < 12; i++) {
+                    val += CPUhelper(x * freq / GRID_SIZE, y * freq / GRID_SIZE) * amp;
+
+                    freq *= 2;
+                    amp /= 2;
+                }
+
+                //contrast
+                val *= 1.2;
+
+                //clipping
+                if (val > 1.0f) {
+                    val = 1.0f;
+                } else if (val < -1.0f) {
+                    val = -1.0f;
+                }
+
+                //convert 1 to -1 into 255 to 0
+                int color = (int)(((val + 1.0f) * 0.5f) * 255);
+                pixels[index] = color;
+                pixels[index + 1] = color;
+                pixels[index + 2] = color;
+                pixels[index + 3] = 255;
+            }
+        }
     }
 
-    void perlinNoiseCPU() {
 
-    }
+    void SIMD() { }
 
-    void perlinNoiseSIMD() { }
-
-    void perlinNoiseGPU() {
+    #define PERLIN_WIDTH 0
+    void GPU() {
         std::string perlinCode { raylib::LoadFileText("perlinNoise.glsl") };
-        unsigned int perlinShader = rlCompileShader(perlin.c_str(), RL_COMPUTE_SHADER);
+        unsigned int perlinShader = rlCompileShader(perlinCode.c_str(), RL_COMPUTE_SHADER);
         unsigned int perlinProgram = rlLoadComputeShaderProgram(perlinShader);
 
 
@@ -314,7 +428,80 @@ public:
         //... itd
     }
 
+private:
+    std::vector<uint8_t> pixels;
+
+public:
+
+    raylib::Texture2DUnmanaged texture;
+
+    PerlinNoise(raylib::Window& window) : permutation(), texture(){
+        permutation.reserve(512);
+        for (size_t i = 0; i < 256; i++) {
+            permutation.emplace_back(i);
+        }
+        assert(permutation.capacity() == 512);
+
+        FisherYatesShuffle(permutation);
+
+        for (size_t i = 0; i < 256; i++) {
+            permutation.emplace_back(permutation[i]);
+        }
+        assert(permutation.size() == permutation.capacity() && permutation.capacity() == 512);
+
+        int width = window.GetWidth();
+        int height = window.GetHeight();
+
+        pixels.resize(height * width * 4);
+        assert(pixels.size() == height * width * 4);
+
+        //
+        //// change to CPU simd or GPU later when implemented
+        //
+        CPU(pixels, width, height);
+        assert(pixels.size() == height * width * 4);
+
+        //raylib::Image gives black image/texture!!
+        Image image = { 0 };
+        image.height = height;
+        image.data = pixels.data();
+        image.format = PIXELFORMAT_UNCOMPRESSED_R8G8B8A8;
+        image.mipmaps = 1;
+        image.width = width;
+        
+        texture = raylib::Texture2DUnmanaged { image };
+    }
+
     void draw() {
+        texture.Draw();
+    }
+
+    ~PerlinNoise() {
+        texture.Unload();
+    }
+};
+
+class Game {
+public:
+    Line line;
+    Background background;
+    raylib::Camera2D camera;
+
+private:
+    GameThreadPool gtp;
+    PerlinNoise perlinNoise;
+
+public:
+
+    Game(raylib::Window& window) : line(window, window.GetWidth(), window.GetHeight()), background(window, raylib::Color { 31, 31, 30, 255 }),
+                                   camera({ 0, 0 }, {0,0}, 0, 0), gtp(window, line, background, [this]() { this->draw(); }, background.color),
+                                   perlinNoise(window)
+    {
+
+    }
+
+    void draw() {
+        perlinNoise.draw();
         background.draw();
         line.draw(raylib::Color::RayWhite());
     }
